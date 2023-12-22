a8b341de96e4caecb535baa5813bb314f1d9c532
FursovaMashaa <fursovamaria1@gmail.com>
2023-12-23 01:53:46 +0400

    the third point had been completed

--- numb_3.py
+++ numb_3.py
@@ -1,38 +1,44 @@
 import os
 import csv
 import shutil
-from typing import List
-import random
+from random import sample
 
 
-def name_change(past_name: str, new: str, file_types: List[str]) -> None:
+def copy_dataset_with_random_numbers(past_name: str, new: str) -> None:
     absolute_path = os.path.abspath(new)
     relative_path = os.path.relpath(new)
-    for file_type in file_types:
-        path = os.path.join(os.path.abspath(past_name), file_type)
+    random_number = sample(range(0, 10000), 1988)
+    counter = 0
+    for name in os.listdir(past_name):
+        path = os.path.join(os.path.abspath(past_name), name)
         list_img = os.listdir(path)
         for img in list_img:
-            # Генерируем случайный номер
-            random_num = random.randint(0, 10000)
-            # Получаем расширение файла
-            ext = os.path.splitext(img)[1]
-            # Создаем новое имя файла в формате "номер.расширение"
-            new_name = f"{random_num}{ext}"
-            # Копируем файл с новым именем
-            shutil.copy(os.path.join(path, img), os.path.join(new, new_name))
-            # Записываем информацию о файле в файл-аннотацию
-            with open('annotation_3.csv', 'a', newline='') as f_csv:
-                writer = csv.writer(f_csv, delimiter=',')
-                writer.writerow([os.path.join(absolute_path, new_name),
-                                 os.path.join(relative_path, new_name), file_type])
+            new_n = str(random_number[counter]).zfill(4)
+            shutil.copy(os.path.join(path, img), os.path.join(new, F'{new_n}.jpg'))
+            with open("annotation_3.csv", "a") as f:
+                writer = csv.writer(f, delimiter=",", lineterminator="\r")
+                writer.writerow([os.path.join(absolute_path, F'{new_n}.jpg'),
+                                 os.path.join(relative_path, F'{new_n}.jpg'), name])
+                counter += 1
 
 def main() -> None:
-    class_name = ['']
+    
     past_title = 'dataset_1'
     new_title = 'dataset_3'
+    
     if not os.path.isdir(new_title):
         os.mkdir(new_title)
-    name_change(past_title, new_title, class_name)
+
+    copy_dataset_with_random_numbers(past_title, new_title)
 
 if __name__ == "__main__":
     main()
+
+
+'''
+Написать скрипт, создающий копию датасета таким образом, 
+чтобы каждый файл из сходного датасета получил случайный 
+номер от 0 до 10000, и датасет представлял собой следующую 
+структуру dataset/номер.jpg. Для того чтобы осталась возможность 
+определить принадлежность экземпляра к классу создать файл-аннотацию.
+'''
