9256727286bf98cf00c2aaf752cd84ed61b575dc
FursovaMashaa <fursovamaria1@gmail.com>
2023-12-22 20:25:56 +0400

    get_relative_path

--- numb_1.py
+++ numb_1.py
@@ -13,19 +13,30 @@ def get_absolute_path(class_img: str ) -> List[str]:
     return image_abs_path
 
 
+def get_relative_path(class_img: str) -> List[str]:
+   
+    rel_path = os.path.relpath("dataset_1")
+    class_path = os.path.join(rel_path, class_img)
+    image_names = os.listdir(class_path)
+    image_rel_path = [os.path.join(class_path, name)for name in image_names]
+
+    return image_rel_path
+
+
 def main() -> str:
 
     cat_abs_paths = get_absolute_path('cat')
+    cat_rel_paths = get_relative_path('cat')
     dog_abs_paths = get_absolute_path('dog')
-    
+    dog_rel_paths = get_relative_path('dog')
 
     with open('annotasion_1.csv', 'w') as csv_file:
         writer = csv.writer(csv_file, delimiter=',', lineterminator='\r')
-        writer.writerow(['Absolute Path', 'Class Name'])
-        for abs_path  in zip(cat_abs_paths):
-            writer.writerow([abs_path, 'cat'])
-        for abs_path in zip(dog_abs_paths,):
-            writer.writerow([abs_path, 'dog'])
+        writer.writerow(['Absolute Path', 'Relative Path', 'Class Name'])
+        for abs_path, rel_path in zip(cat_abs_paths, cat_rel_paths):
+            writer.writerow([abs_path, rel_path, 'cat'])
+        for abs_path, rel_path in zip(dog_abs_paths, dog_rel_paths):
+            writer.writerow([abs_path, rel_path, 'dog'])
 
 
 if __name__ == "__main__":
