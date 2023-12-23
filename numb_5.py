68fbf108dc8c90b4f7fc1c09f7bb41fa8b47eb0e
FursovaMashaa <fursovamaria1@gmail.com>
2023-12-23 14:32:42 +0400

    update numb_5 and complete work

--- numb_5.py
+++ numb_5.py
@@ -1,19 +1,31 @@
-class SimpleIterator:
-    def __init__(self, limit):
-        self.limit = limit
-        self.counter = 0
+import os
+
+
+class InstanceIterator:
+    def __init__(self, class_label: str):
+        self.class_label = class_label
+        self.path = os.path.join("dataset_1", class_label)
+        self.names = os.listdir(self.path)
+        self.names.append(None)
 
     def __iter__(self):
         return self
 
     def __next__(self):
-        if self.counter < self.limit:
-            self.counter += 1
-            return 1
+        if self.names:
+            name = self.names.pop(0)
+            if name is not None:
+                return os.path.join(self.path, name)
+            else:
+                raise StopIteration
         else:
             raise StopIteration
 
-s_iter1 = SimpleIterator(3)
+def main() -> None:
+    class_label = "cat"
+    iterator = InstanceIterator(class_label)
+    for instance, _ in zip(iterator, range(10)):
+        print(instance)
 
-for val in s_iter1:
-  print(val)
+if __name__ == "__main__":
+    main()
